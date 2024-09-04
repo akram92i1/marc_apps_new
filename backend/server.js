62c29e2d30ea4eccb32954f475c108821f31e78e
KHELIL Akram <khelili.akram@courrier.uqam.ca>
2024-09-04 00:03:48 -0400

    add cors to semerlepresent 10

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ const path = require('path');
 const cors = require('cors');
 const userRoutes = require('./routes/user');
 const messsageRoutes = require('./routes/message.routes');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const app = express();
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
+console.log('JWT_SECRET:', process.env.JWT_SECRET); // Debug log
 // CORS Middleware
 app.use(cors({
   origin: '*',
@@ -22,6 +27,14 @@ app.use(express.static(path.join(__dirname, '../build')));
 // API Routes
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messsageRoutes);
+
+// MongoDB Connection
+mongoose.connect(`mongodb+srv://admin:Tbm930antonov2@marcdatabase.rcgfo.mongodb.net/marc-database?retryWrites=true&w=majority`, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
 
 // Wildcard route to serve the React app for any unmatched routes
 app.get('/*', (req, res) => {
