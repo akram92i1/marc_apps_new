34ff6a74a75af646d60288210eb0959989174342
akram92i1 <akramkhelili1996@gmail.com>
2024-09-08 05:35:20 -0400

    change test the backend response 5 !

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,40 +4,71 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
 const User = require('../models/User');
+// router.post('/login', async (req, res) => {
+//     const { email, password } = req.body;
+//     console.log("--> email" , email);
+//     console.log("--> password" , password); 
+//   try {
+//       let user = await User.findOne({ email });
+//       if (!user) {
+//         console.log("--> no user was found");
+//         return res.status(400).json({ msg: 'Invalid credentials' });
+//       }
+//       bcrypt.compare(password, user.password).then((validPassword) => {
+//       if (validPassword) {
+//         console.log('Passwords match!');
+//         } else {
+//            console.log("--> Invalid credentials");                                                                                                                                                        
+//            return res.status(400).json({ msg: 'Invalid credentials' }); 
+//       }
+//       });
+//       const payload = {
+//         user: {
+//           id: user.id,
+//         },
+//       };
+  
+//       const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
+//         if (err) throw err;
+//         return res.json({ token });
+//       });
+  
+//     } catch (err) {
+//       console.error(err.message);
+//       return res.status(500).send( {message:'Server error'});
+//     }
+//   });
+
+
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    console.log("--> email" , email);
-    console.log("--> password" , password); 
+  const { email, password } = req.body;
   try {
-      let user = await User.findOne({ email });
-      if (!user) {
-        console.log("--> no user was found");
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+    bcrypt.compare(password, user.password).then((validPassword) => {
+      if (!validPassword) {
         return res.status(400).json({ msg: 'Invalid credentials' });
       }
-      bcrypt.compare(password, user.password).then((validPassword) => {
-      if (validPassword) {
-        console.log('Passwords match!');
-        } else {
-           console.log("--> Invalid credentials");                                                                                                                                                        
-           return res.status(400).json({ msg: 'Invalid credentials' }); 
-      }
-      });
       const payload = {
         user: {
           id: user.id,
         },
       };
-  
-      const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
+      jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
         if (err) throw err;
         return res.json({ token });
       });
-  
-    } catch (err) {
+    }).catch((err) => {
       console.error(err.message);
-      return res.status(500).send( {message:'Server error'});
-    }
-  });
+      return res.status(500).send({ message: 'Server error' });
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send({ message: 'Server error' });
+  }
+}); 
 
   // Sign up route
 router.post('/signup', async (req, res) => {
