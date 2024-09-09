a5fe0f74d54674c9895b5680499dda46dfe10c11
akram92i1 <akramkhelili1996@gmail.com>
2024-09-09 18:44:17 -0400

    update the server.js file for handeling token 5 !

--- backend/server.js
+++ backend/server.js
@@ -60,9 +60,27 @@ mongoose.connect(`mongodb+srv://admin:Tbm930antonov2@marcdatabase.rcgfo.mongodb.
 // Wildcard route to serve the React app for any unmatched routes
 const reactApp = path.join(__dirname, '../projectManager/build'); 
 app.use(express.static(reactApp));
-app.get('*', (req, res) => { 
-  const token = req.cookies.token ;
-  console.log("here is done information about token --->" ,token);
+// app.get('*', (req, res) => { 
+//   const token = req.cookies.token ;
+//   console.log("here is done information about token --->" ,token);
+//   if (!token) {
+//     return res.redirect('/'); // Redirect to login page
+//   }
+//   try {
+//     const decoded = jwt.verify(token, JWT_SECRET);
+//     req.user = decoded.user;
+//     res.sendFile(path.join(reactApp)); // Serve the React app
+//   } catch (err) {
+//     return res.redirect('/'); // Redirect to login page
+//   }
+// });
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+app.get('/verify-token', (req, res) => {
+  const token = req.cookies.token;
+  console.log("here is done information about token --->", token);
   if (!token) {
     return res.redirect('/'); // Redirect to login page
   }
@@ -74,7 +92,4 @@ app.get('*', (req, res) => {
     return res.redirect('/'); // Redirect to login page
   }
 });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
